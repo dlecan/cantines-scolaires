6f8cf851e3b9efefaa4475aeca0154ec0f59ee25
Damien Lecan <dev@dlecan.com>
2013-09-12 17:27:07 +0200

    WIP on PDF parsing

--- backend/test/PDFParser.scala
+++ backend/test/PDFParser.scala
@@ -1,30 +1,54 @@
-import java.io.FileOutputStream
-import java.io.IOException
+import com.itextpdf.text.Rectangle
 import java.io.PrintWriter
 
 import com.itextpdf.text.pdf.PdfReader
-import com.itextpdf.text.pdf.parser.{LocationTextExtractionStrategy, PdfReaderContentParser, SimpleTextExtractionStrategy, TextExtractionStrategy}
+import com.itextpdf.text.pdf.parser._
 
 object ExtractPageContent {
 
+  val xOffset = 59
+  val yOffset = 48
+
+  val rowWidth = 159
+  val colHeigth = 119
+
   def parsePdf(pdf: String) = {
     val reader = new PdfReader(pdf)
-    val parser = new PdfReaderContentParser(reader)
-    val out = new PrintWriter(System.out)
 
-    println("Number of pages found: " + reader.getNumberOfPages)
+    for {
+      row <- 0 to 3
+      col <- 0 to 4
+    } yield {
+      val rect = getRectangle(row, col)
+      println(s"($row, $col) =================================================================================")
+      println(rect.betterToString)
+      println()
 
-    (1 to reader.getNumberOfPages).foreach {
-      i =>
-        println(s"Page number $i")
-        val strategy = parser.processContent(i, new LocationTextExtractionStrategy)
-        out.println(strategy.getResultantText())
+      val filter = new RegionTextRenderFilter(rect)
+      val strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter)
+
+      println(PdfTextExtractor.getTextFromPage(reader, 1, strategy))
+      println("=================================================================================")
+      println()
     }
-    out.flush()
-    out.close()
+    reader.close()
   }
 
   def main(args: Array[String]) = {
     ExtractPageContent.parsePdf(getClass.getResource("/menu.pdf").toExternalForm)
   }
+
+  def getRectangle(row: Int, col: Int) = {
+    def xRow(x: Int) = xOffset + x * colHeigth
+    def yCol(y: Int) = yOffset + y * rowWidth
+
+    new Rectangle(xRow(row), yCol(col), xRow(row + 1), yCol(col + 1))
+  }
+
+  implicit class RectangleWithBetterToString(rect: Rectangle) {
+    def betterToString() = {
+      "(" + rect.getLeft + ", " + rect.getBottom + "), (" + rect.getRight + ", " + rect.getTop + ")"
+    }
+  }
+
 }
