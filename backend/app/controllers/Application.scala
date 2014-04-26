f78db52c101867bf49202f45dfd8a284888d989b
Damien Lecan <dev@dlecan.com>
2014-04-26 22:58:10 +0200

    Simplification

--- backend/app/controllers/Application.scala
+++ backend/app/controllers/Application.scala
@@ -4,10 +4,8 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+
+  def menusCetteSemaine() = TODO
 
   def menus(from: Option[String] = None, to: Option[String] = None) = TODO
   
